8540e4bf9955f30fdd6571c6240b25f4866fc03c
fwchen <me@chenfangwei.com>
2020-04-13 11:46:12 +0800

    update

--- src/Page/Space/SpaceSide/SpaceSide.tsx
+++ src/Page/Space/SpaceSide/SpaceSide.tsx
@@ -3,10 +3,14 @@ import { SpaceDisplay } from '../../../interface/space';
 import { List } from '../../../Component/List/List';
 import { ListSection } from '../../../Component/List/ListSection';
 import { ListItem } from '../../../Component/List/ListItem';
+import { Space } from '../../../interface/page';
 
-const mockSpaceLists: SpaceDisplay[] = [
-  { id: '1', name: '猕猴桃空间' },
-  { id: '2', name: '秦朝城市规划研究' },
+const mockSpaceLists: Space[] = [
+  { id: '1', name: '秦朝年历表' },
+  { id: '2', name: '嬴政的一生' },
+  { id: '3', name: '阿房宫真的存在吗？' },
+  { id: '4', name: '秦二世的后宫有多少个？' },
+  { id: '5', name: '兵马俑中的稀世珍品' },
 ];
 
 export function SpaceSide() {
