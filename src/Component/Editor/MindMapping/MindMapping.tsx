b291663fe1df1827d2586444155edb69cfe721d4
fwchen <me@chenfangwei.com>
2020-04-16 00:08:04 +0800

    update

--- src/Component/Editor/MindMapping/MindMapping.tsx
+++ src/Component/Editor/MindMapping/MindMapping.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { generateD3Node } from './MindMappingController';
 
 export function MindMapping() {
