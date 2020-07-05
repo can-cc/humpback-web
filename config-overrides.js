30a8c8f180f91ef7bcdf4da71749e418500742a9
fwchen <me@chenfangwei.com>
2020-07-05 13:06:58 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,8 @@ module.exports = function override(config, env) {
   //do stuff with the webpack config...
   config.plugins.forEach((plugin) => {
     if (plugin.constructor.name === 'GenerateSW') {
-      plugin.config.exclude.push(/$oauth2/);
-      plugin.config.navigateFallbackBlacklist.push(/$oauth2/);
+      plugin.config.exclude.push(/$\/oauth2/);
+      plugin.config.navigateFallbackBlacklist.push(/$\/oauth2/);
     }
   })
   return config;
