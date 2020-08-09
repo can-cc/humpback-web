bc2fe6b7379522ada16472c267d1e432cce5a8a6
fwchen <me@chenfangwei.com>
2020-08-09 23:40:47 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,9 @@ module.exports = function override(config, env) {
   config.plugins.forEach((plugin) => {
     if (plugin.constructor.name === 'GenerateSW') {
       plugin.config.exclude.push(/^\/oauth2/);
+      plugin.config.exclude.push(/^\/login/);
       plugin.config.navigateFallbackBlacklist.push(/^\/oauth2/);
+      plugin.config.navigateFallbackBlacklist.push(/^\/login/);
     }
   })
   return config;
